38f978c24feab5f2020f5e0e30b889df13421f32
Benjamin Gonzalez <bengonzalezre@gmail.com>
2025-06-18 10:37:17 -0500

    Adding templates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mypickup_django
+
+
+### Starter template for bootstrap
+[Bootstrap Starter Template](https://getbootstrap.com/docs/4.0/getting-started/introduction/#starter-template)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        "author": "BenG",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "June 17, 2025"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "June 18, 2025"
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {"title": "About"})
