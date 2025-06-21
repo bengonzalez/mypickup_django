29f7bd94239a47eed7af93913beb5299bf8dd750
Benjamin Gonzalez <bengonzalezre@gmail.com>
2025-06-21 15:41:50 -0500

    Database migrations

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-    {
-        "author": "BenG",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "June 17, 2025"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "June 18, 2025"
-    }
-]
-
-# Create your views here.
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
