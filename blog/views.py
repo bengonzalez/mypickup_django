9e19fefaf4b42a2905a444f51afd537f90df80c5
Benjamin Gonzalez <bengonzalezre@gmail.com>
2025-07-23 21:11:08 -0500

    Latest changes on part 10 of tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 def home(request):
@@ -7,6 +9,63 @@ def home(request):
         "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'object'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+        # Uncomment the following lines if you want to restrict access to the author only
+        if self.request.user == post.author:
+            return True
+        else:
+            # Optionally, you can redirect or raise an error
+            # return HttpResponseForbidden("You are not allowed to edit this post.")
+            # Or simply return False
+            # This will prevent the user from accessing the update view if they are not the author
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            # Optionally, you can redirect or raise an error
+            # return HttpResponseForbidden("You are not allowed to edit this post.")
+            # Or simply return False
+            # This will prevent the user from accessing the update view if they are not the author
+            return False
 
 def about(request):
     return render(request, 'blog/about.html', {"title": "About"})
